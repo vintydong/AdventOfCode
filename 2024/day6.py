6bdc054d8622404620f8dd6d9bd6b6ff177040fb
Vinty <vintydong910@gmail.com>
2024-12-07 21:56:58 -0500

    Fix day6 part 2

--- 2024/day6.py
+++ 2024/day6.py
@@ -30,6 +30,29 @@ def part1(data):
         x, y = x + dx, y + dy
     return len(seen)
 
+def check_loop(start, data, obstacle, current_dir):
+    seen = {}
+    m, n = len(data), len(data[0])
+    y, x = start
+    dirs = [(0,-1),(1,0),(0,1),(-1,0)]
+    dx, dy = dirs[current_dir]
+    while -1 < x < n and -1 < y < m:
+        if data[y][x] == '#' or (y,x) == obstacle:
+            # keep track of which dir this square was visited
+            x, y = x - dx, y - dy
+            current_dir = (current_dir + 1) % 4
+            continue
+        if (x,y) in seen:
+            # if same dir, then we've taken this path before => loop
+            if current_dir in seen[(x,y)]:
+                return True
+            seen[(x,y)].add(current_dir)
+        else:
+            seen[(x,y)] = set([current_dir])
+        dx, dy = dirs[current_dir]
+
+        x, y = x + dx, y + dy
+    return False
 
 def part2(data):
     """Solve part 2."""
@@ -55,25 +78,14 @@ def part2(data):
         seen.add((x,y))
         dx, dy = dirs[current_dir]
 
-        # if "right" was seen, check if this was a path
-        right_dir = dirs[(current_dir+1)%4]
-        right_x = x + right_dir[0]
-        right_y = y + right_dir[1]
-        # if (right_x, right_y) in seen:
-        #     print(f"{right_y}, {right_x} was seen!")
-        seen_counts = 0
-        while 0 < right_y < n and 0 < right_x < m and data[right_y][right_x] != '#':
-            if (right_x, right_y) in seen:
-                # print(f"Early stop at ({right_y}, {right_x})")
-                seen_counts += 1
-            right_x += right_dir[0]
-            right_y += right_dir[1]
-        else:
-            # print(f"Stop at ({right_y}, {right_x})")
-            # print(f"Adding {(x+dx,y+dy)}")
-            if seen_counts > 1:
+        # at every step, simulate going right
+        # except if it blocks the path already taken
+        if (x+dx, y+dy) not in seen and (x+dx, y+dy) not in obstacles:
+            right_dir = (current_dir+1) % 4
+            # print("Checking obstacle at", (y+dy, x+dx))
+            if check_loop((y, x), data, (y+dy, x+dx), right_dir):
                 obstacles.add((x+dx,y+dy))
-        
+
         x, y = x + dx, y + dy
     return len(obstacles)
 
